themicksa/mediawiki-extensions

commit 5dfcd87812b7f492b3569aec1c04c53c83412191

Use date and time separately in UI sentence 'centralauth-admin-blocked'
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@48057 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

CentralAuth/CentralAuth.i18n.php
@@ -140,7 +140,7 @@ If the global account is yours too, you can merge this account if you type the g
 	'centralauth-admin-unattached'         => 'Unattached accounts',
 	'centralauth-admin-no-unattached'      => 'No unmerged accounts remain.',
 	'centralauth-admin-notblocked'         => 'Not blocked',
-	'centralauth-admin-blocked'			   => 'Blocked with expiry $1. Reason: $2',
+	'centralauth-admin-blocked'            => 'Blocked with expiry on $3 at $4. Reason: $2',
 	'centralauth-admin-list-localwiki'     => 'Local wiki',
 	'centralauth-admin-list-attached-on'   => 'Attached on',
 	'centralauth-admin-list-method'        => 'Method',

CentralAuth/SpecialCentralAuth.php
@@ -342,9 +342,11 @@ class SpecialCentralAuth extends SpecialPage {
 		if ($row['blocked']) {
 			global $wgLang;
 			$expiry = $wgLang->timeanddate( $row['block-expiry'] );
+			$expiryd = $wgLang->date( $row['block-expiry'] );
+			$expiryt = $wgLang->time( $row['block-expiry'] );
 			$reason = $row['block-reason'];
 			
-			return wfMsgExt( 'centralauth-admin-blocked', 'parseinline', array( $expiry, $reason ) );
+			return wfMsgExt( 'centralauth-admin-blocked', 'parseinline', array( $expiry, $reason, $expiryd, $expiryt ) );
 		} else {
 			return wfMsgExt( 'centralauth-admin-notblocked', 'parseinline' );
 		}

commit 22c0854ca806213b2b6c3485b47079bc92707744

Fix blocked status display, was broken for indef blocks since r48057.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@48853 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

CentralAuth/CentralAuth.i18n.php
@@ -140,7 +140,8 @@ If the global account is yours too, you can merge this account if you type the g
 	'centralauth-admin-unattached'         => 'Unattached accounts',
 	'centralauth-admin-no-unattached'      => 'No unmerged accounts remain.',
 	'centralauth-admin-notblocked'         => 'Not blocked',
-	'centralauth-admin-blocked'            => 'Blocked with expiry on $3 at $4. Reason: $2',
+	'centralauth-admin-blocked'            => 'Blocked, expires $3 at $4. Reason: $2',
+	'centralauth-admin-blocked-indef'      => 'Blocked indefinitely. Reason: $1',
 	'centralauth-admin-list-localwiki'     => 'Local wiki',
 	'centralauth-admin-list-attached-on'   => 'Attached on',
 	'centralauth-admin-list-method'        => 'Method',

CentralAuth/SpecialCentralAuth.php
@@ -340,13 +340,18 @@ class SpecialCentralAuth extends SpecialPage {
 	
 	function showBlockStatus( $row ) {
 		if ($row['blocked']) {
-			global $wgLang;
-			$expiry = $wgLang->timeanddate( $row['block-expiry'] );
-			$expiryd = $wgLang->date( $row['block-expiry'] );
-			$expiryt = $wgLang->time( $row['block-expiry'] );
+			if ($row['block-expiry'] == 'infinity') {
 			$reason = $row['block-reason'];
-			
-			return wfMsgExt( 'centralauth-admin-blocked', 'parseinline', array( $expiry, $reason, $expiryd, $expiryt ) );
+				return wfMsgExt( 'centralauth-admin-blocked-indef', 'parseinline', array( $reason ) );
+			} else {
+				global $wgLang;
+				$expiry = $wgLang->timeanddate( $row['block-expiry'] );
+				$expiryd = $wgLang->date( $row['block-expiry'] );
+				$expiryt = $wgLang->time( $row['block-expiry'] );
+				$reason = $row['block-reason'];
+				
+				return wfMsgExt( 'centralauth-admin-blocked', 'parseinline', array( $expiry, $reason, $expiryd, $expiryt ) );
+			}
 		} else {
 			return wfMsgExt( 'centralauth-admin-notblocked', 'parseinline' );
 		}