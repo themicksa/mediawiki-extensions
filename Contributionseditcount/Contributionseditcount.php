themicksa/mediawiki-extensions

commit f6f5dfd44bbebfbd47141840cecf09220ce7e4c2

Followup to r51699: Actually parse the message, wfMsgExt() by itself doesn't do anything :) Also, refactor this to use the hook directly, rather than calling the function and making an object on every request.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@51712 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

Contributionseditcount/Contributionseditcount.php
@@ -8,44 +8,29 @@ if (!defined('MEDIAWIKI')) die();
  * @bug 1725
  *
  * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+ * @author Chad Horohoe <chad@anyonecanedit.org>
  * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
-$wgExtensionFunctions[] = 'wfContributionseditcount';
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'Contributionseditcount',
 	'description' => 'Displays an edit count on Special:Contributions',
 	'descriptionmsg' => 'contributionseditcount-desc',
-	'author' => 'Ævar Arnfjörð Bjarmason',
+	'author' => array( 'Ævar Arnfjörð Bjarmason', 'Chad Horohoe' ),
 	'url' => 'http://www.mediawiki.org/wiki/Extension:Contributionseditcount',
 );
-
 $wgExtensionMessagesFiles['Contributionseditcount'] = dirname( __FILE__ ) . '/Contributionseditcount.i18n.php';
+$wgHooks['SpecialContributionsBeforeMainOutput'][] = 'wfContributionseditcount';
 
-function wfContributionseditcount() {
-
-	wfUsePHP( 5.0 );
-	wfUseMW( '1.6alpha' );
-
-	class Contributionseditcount {
-
-		public function __construct() {
-			global $wgHooks;
-			$wgHooks['SpecialContributionsBeforeMainOutput'][] = array( &$this, 'hook' );
-		}
-
-		public function hook( $uid ) {
-			global $wgOut, $wgLang;
-			if ( $uid != 0 )
-				wfLoadExtensionMessages( 'Contributionseditcount' );
-				$wgOut->addWikiText( wfMsgExt( 'contributionseditcount', array(),
+function wfContributionseditcount( $uid ) {
+	if ( $uid != 0 )
+		global $wgOut, $wgLang;
+		wfLoadExtensionMessages( 'Contributionseditcount' );
+		$wgOut->addWikiText( wfMsgExt( 'contributionseditcount', array( 'parsemag' ),
 						$wgLang->formatNum( User::edits( $uid ) ),
 						User::whoIs( $uid ) ) );
-			return true;
-		}
+		return true;
 	}
-
-	new Contributionseditcount();
 }

commit 405f948528d7cb4b9ea6ff51b2c39c8995ce3327

Fix syntax error introduced in r51712 (but likely provoked by an error introduced in r29575)
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@51732 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

Contributionseditcount/Contributionseditcount.php
@@ -25,7 +25,7 @@ $wgExtensionMessagesFiles['Contributionseditcount'] = dirname( __FILE__ ) . '/Co
 $wgHooks['SpecialContributionsBeforeMainOutput'][] = 'wfContributionseditcount';
 
 function wfContributionseditcount( $uid ) {
-	if ( $uid != 0 )
+	if ( $uid != 0 ) {
 		global $wgOut, $wgLang;
 		wfLoadExtensionMessages( 'Contributionseditcount' );
 		$wgOut->addWikiText( wfMsgExt( 'contributionseditcount', array( 'parsemag' ),