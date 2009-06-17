themicksa/mediawiki-extensions

commit a834ddfaebe7f3304a35848a60f7dc6e8bf75d30

* makeLinkObj() -> link() * update command line option documentation
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@51835 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

Translate/TranslatePage.php
@@ -302,11 +302,15 @@ class SpecialTranslate extends SpecialPage {
 	private function makeOffsetLink( $label, $offset ) {
 		global $wgUser;
 		$skin = $wgUser->getSkin();
-		$link = $skin->makeLinkObj( $this->getTitle(), $label,
-			wfArrayToCGI(
-				array( 'offset' => $offset ),
-				$this->nondefaults
-			)
+		$query = array_merge(
+			'offset' => $offset,
+			$this->nondefaults
+		);
+		$link = $skin->link(
+			$this->getTitle(),
+			$label,
+			array(),
+			$query
 		);
 		return $link;
 	}

Translate/scripts/fuzzy.php
@@ -19,8 +19,8 @@ Fuzzy bot command line script
 Usage: php fuzzy.php [options...] <messages>
 
 Options:
-  --really        Don't just run dry-run
-  --skiplanguages Skip some languages
+  --really        Really fuzzy, no dry-run
+  --skiplanguages Skip some languages (comma separated)
   --comment       Comment for updating
 
 EOT

commit 0e221c11ae285b0f178152f38ce225137abad8e9

Fix paging broken in r51835.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@52063 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

Translate/TranslatePage.php
@@ -303,8 +303,8 @@ class SpecialTranslate extends SpecialPage {
 		global $wgUser;
 		$skin = $wgUser->getSkin();
 		$query = array_merge(
-			array( 'offset' => $offset ),
-			$this->nondefaults
+			$this->nondefaults,
+			array( 'offset' => $offset )
 		);
 		$link = $skin->link(
 			$this->getTitle(),