themicksa/mediawiki-extensions

commit 6502e6541fd61a411189f80cb03c382703dca848

Short-circuited template stuff for API unapprove
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@52882 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

FlaggedRevs/api/ApiReview.php
@@ -82,24 +82,26 @@ class ApiReview extends ApiBase {
 		if( !$title->quickUserCan('edit') || !RevisionReview::userCanSetFlags($form->dims,$form->oflags,$form->config) )
 			$this->dieUsage( "You don't have the necessary rights to set the specified flags.", 'permissiondenied' );
 
-		// Now get the template and image parameters needed
-		// If it is the current revision, try the parser cache first
-		$article = new Article( $title, $revid );
-		if( $rev->isCurrent() ) {
-			$parserCache = ParserCache::singleton();
-			$parserOutput = $parserCache->get( $article, $wgUser );
+		if( $form->approve ) {
+			// Now get the template and image parameters needed
+			// If it is the current revision, try the parser cache first
+			$article = new Article( $title, $revid );
+			if( $rev->isCurrent() ) {
+				$parserCache = ParserCache::singleton();
+				$parserOutput = $parserCache->get( $article, $wgUser );
+			}
+			if( empty( $parserOutput ) ) {
+				// Miss, we have to reparse the page
+				global $wgParser;
+				$text = $article->getContent();
+				$options = FlaggedRevs::makeParserOptions();
+				$parserOutput = $wgParser->parse( $text, $title, $options );
+			}
+			// Set version parameters for review submission
+			list( $form->templateParams, $form->imageParams, $form->fileVersion ) =
+				FlaggedRevs::getIncludeParams( $article, $parserOutput->mTemplateIds, $parserOutput->fr_ImageSHA1Keys );
 		}
-		if( empty( $parserOutput ) ) {
-			// Miss, we have to reparse the page
-			global $wgParser;
-			$text = $article->getContent();
-			$options = FlaggedRevs::makeParserOptions();
-			$parserOutput = $wgParser->parse( $text, $title, $options );
-		}
-
-		list( $form->templateParams, $form->imageParams, $form->fileVersion ) =
-			FlaggedRevs::getIncludeParams( $article, $parserOutput->mTemplateIds, $parserOutput->fr_ImageSHA1Keys );
-
+		
 		// Do the actual review
 		list( $approved, $status ) = $form->submit();
 		if( $status === true ) {

commit 03630b49b0451582d724728473e7897937ee3373

bug 20705: fixed api review; regression from r52882
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@56576 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

FlaggedRevs/api/ApiReview.php
@@ -82,7 +82,7 @@ class ApiReview extends ApiBase {
 		if( !$title->quickUserCan('edit') || !RevisionReview::userCanSetFlags($form->dims,$form->oflags,$form->config) )
 			$this->dieUsage( "You don't have the necessary rights to set the specified flags.", 'permissiondenied' );
 
-		if( $form->approve ) {
+		if( $form->isApproval() ) {
 			// Now get the template and image parameters needed
 			// If it is the current revision, try the parser cache first
 			$article = new Article( $title, $revid );

FlaggedRevs/specialpages/RevisionReview_body.php
@@ -379,17 +379,22 @@ class RevisionReview extends UnlistedSpecialPage
 		$revlink = $this->skin->makeLinkObj( $this->page, $date, 'oldid=' . $rev->getId() );
 		return "<li>$difflink $revlink " . $this->skin->revUserLink($rev) . " " . $this->skin->revComment($rev) . "</li>";
 	}
+	
+	public function isApproval() {
+		# If all values are set to zero, this has been unapproved
+		if( !count( FlaggedRevs::getDimensions() ) && $this->approve ) {
+			return true; // no tags & approve param given
+		}
+		foreach( $this->dims as $quality => $value ) {
+			if( $value ) return true;
+		}
+		return false;
+	}
 
 	public function submit() {
 		global $wgUser;
 		# If all values are set to zero, this has been unapproved
-		$approved = !count( FlaggedRevs::getDimensions() ) && $this->approve;
-		foreach( $this->dims as $quality => $value ) {
-			if( $value ) {
-				$approved = true;
-				break;
-			}
-		}
+		$approved = $this->isApproval();
 		# Double-check permissions
 		if( !$this->page->quickUserCan('edit') || !self::userCanSetFlags($this->dims,$this->oflags,$this->config) ) {
 			return array($approved,false);