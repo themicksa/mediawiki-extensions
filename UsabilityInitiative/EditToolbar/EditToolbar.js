themicksa/mediawiki-extensions

commit 366270301d8e1d5917a253f60380aa638551eecf

EditToolbar: Use $wgUrlProtocols to determine if something is a URL
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@57982 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

UsabilityInitiative/EditToolbar/EditToolbar.js
@@ -779,11 +779,14 @@ js2AddOnloadHook( function() {
 		init: function() {
 			function isExternalLink( s ) {
 				// The following things are considered to be external links:
-				// * Starts with one or more letters followed by ://
+				// * Starts a URL protocol
 				// * Starts with www.
 				// * Ends with a . followed by two or more letters
 				// * Contains a . followed by two or more letters followed by /
-				return s.match( /(^[a-z]+:\/\/)|(^www\.)|([^.]\.[a-z]{2,}($|\/))/i );
+				if ( typeof arguments.callee.regex == 'undefined' )
+					// Cache the regex
+					arguments.callee.regex = new RegExp( "(^(" + urlprotocols + "))|(^www\\.)|([^.]\\.[a-z]{2,}($|\\/))", 'i');
+				return s.match( arguments.callee.regex );
 			}
 			// Updates the UI to show if the page title being inputed by the user exists or not
 			function updateExistence() {

UsabilityInitiative/EditToolbar/EditToolbar.php
@@ -19,7 +19,7 @@
 /* Configuration */
 
 // Bump the version number every time you change any of the .css/.js files
-$wgEditToolbarStyleVersion = 60;
+$wgEditToolbarStyleVersion = 61;
 
 // Set this to true to simply override the stock toolbar for everyone
 $wgEditToolbarGlobalEnable = false;

commit 1609f61a082586b24ce76abcb37cb4caa67e6b24

Correcting bug #21293 introduced in r57982 - removing a trailing comma that was causing problems in IE
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@58170 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

UsabilityInitiative/EditToolbar/EditToolbar.js
@@ -222,7 +222,7 @@ js2AddOnloadHook( function() {
 								ownline: true
 							}
 						}
-					}	,
+					},
 					'indent': {
 						labelMsg: 'edittoolbar-tool-indent',
 						type: 'button',
@@ -230,7 +230,7 @@ js2AddOnloadHook( function() {
 						action: {
 							type: 'encapsulate',
 							options: {
-								pre: ":",
+								pre: ":"
 							}
 						}
 					}

UsabilityInitiative/EditToolbar/EditToolbar.php
@@ -13,13 +13,13 @@
  *
  * @author Trevor Parscal <tparscal@wikimedia.org>, Roan Kattouw <roan.kattouw@gmail.com>
  * @license GPL v2 or later
- * @version 0.1.2
+ * @version 0.1.3
  */
 
 /* Configuration */
 
 // Bump the version number every time you change any of the .css/.js files
-$wgEditToolbarStyleVersion = 62;
+$wgEditToolbarStyleVersion = 63;
 
 // Set this to true to simply override the stock toolbar for everyone
 $wgEditToolbarGlobalEnable = false;
@@ -48,7 +48,7 @@ $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'EditToolbar',
 	'author' => 'Trevor Parscal',
-	'version' => '0.1.2',
+	'version' => '0.1.3',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:UsabilityInitiative',
 	'descriptionmsg' => 'edittoolbar-desc',
 );