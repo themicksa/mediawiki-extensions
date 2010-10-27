themicksa/mediawiki-extensions

commit 8c88a09f484eb1471272726bf5f7b5f8e91e4513

(bug 25339) Allow specifying separate base paths for local and remote paths in ResourceLoaderFileModule::__construct(). Extensions can use this to properly point the resource loader to their resources in a way that doesn't depend on the extensions/ directory being a subdirectory of phase3/ on the filesystem and that respects $wgExtensionAssetsPath. Also converted all extensions using ResourceLoader to use this approach, except MWEmbed which uses way more indirection building its module registrations than I can deal with after midnight.
What actually happens here
* Add $localBasePath, $remoteBasePath params to the FileModule constructor, defaulting to $IP and $wgScriptPath respectively
* Add getLocalPath() and getRemotePath(), which use this information to build a proper FS or URL path for a file
* Make readScriptFiles() non-static so it can use these functions
* Run every single file path we use through either getLocalPath() or getRemotePath() as appropriate
** Except file dependencies, these are already prefixed. Our inability to give them special treatment caused bugs earlier
* Kill prefixFilePathList() and resolveFilePath(), no longer needed


git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@75587 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

Translate/Translate.php
@@ -420,9 +420,10 @@ $wgTranslateYamlLibrary = 'spyc';
 
 # Startup code
 function efTranslateResources( &$resourceLoader ) {
+	global $wgExtensionAssetsPath;
 	$resourceLoader->register( array( 'translate-css' =>
-		new ResourceLoaderFileModule( array( 'styles' => 'extensions/Translate/Translate.css' ) )
-	) );
+		new ResourceLoaderFileModule( array( 'styles' => 'Translate.css' ) )
+	), dirname( __FILE__ ), "$wgExtensionAssetsPath/Translate" );
 	return true;
 }
 

ArticleEmblems/ArticleEmblems.hooks.php
@@ -89,11 +89,12 @@ class ArticleEmblemsHooks {
 	 * ResourceLoaderRegisterModules hook
 	 */
 	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
+		global $wgExtensionAssetsPath;
 		$resourceLoader->register(
 			'ext.articleEmblems',
 			new ResourceLoaderFileModule( array(
-				'styles' => 'extensions/ArticleEmblems/modules/ext.articleEmblems.css',
-			) )
+				'styles' => 'ext.articleEmblems.css',
+			), dirname( __FILE__ ) . '/modules', "$wgExtensionAssetsPath/ArticleEmblems/modules" )
 		);
 		return true;
 	}

ClickTracking/ClickTracking.hooks.php
@@ -83,21 +83,24 @@ class ClickTrackingHooks {
 	 * @return Boolean: always true
 	 */
 	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
+		global $wgExtensionAssetsPath;
+		$localpath = dirname( __FILE__ ) . '/modules';
+		$remotepath = "$wgExtensionAssetsPath/extensions/ClickTracking/modules";
 		$resourceLoader->register( array(
 			'jquery.clickTracking' => new ResourceLoaderFileModule( array(
-				'scripts' => 'extensions/ClickTracking/modules/jquery.clickTracking.js',
+				'scripts' => 'jquery.clickTracking.js',
 				'dependencies' => 'jquery.cookie',
 			) ),
 			'ext.clickTracking' => new ResourceLoaderFileModule( array(
-				'scripts' => 'extensions/ClickTracking/modules/ext.clickTracking.js',
+				'scripts' => 'ext.clickTracking.js',
 				'dependencies' => 'jquery.clickTracking',
 			) ),
 			'ext.clickTracking.special' => new ResourceLoaderFileModule( array(
-				'scripts' => 'extensions/ClickTracking/modules/ext.clickTracking.special.js',
-				'styles' => 'extensions/ClickTracking/modules/ext.clickTracking.special.css',
+				'scripts' => 'ext.clickTracking.special.js',
+				'styles' => 'ext.clickTracking.special.css',
 				'dependencies' => array( 'jquery.ui.datepicker', 'jquery.ui.dialog' ),
 			) ),
-		) );
+		), $localpath, $remotepath );
 		return true;
 	}
 

Maps/Maps.hooks.php
@@ -69,7 +69,7 @@ final class MapsHooks {
 	 * @return true
 	 */
 	public static function registerResourceLoaderModules( ResourceLoader &$resourceLoader ) {
-		global $smwgScriptPath, $wgContLang;
+		global $wgExtensionAssetsPath;
 		
 		$modules = array(	
 		);
@@ -77,7 +77,7 @@ final class MapsHooks {
 		foreach ( $modules as $name => $resources ) { 
 			$resourceLoader->register( $name, new ResourceLoaderFileModule(
 				array_merge_recursive( $resources, array( 'group' => 'ext.maps' ) )
-			) ); 
+			), dirname( __FILE__ ), "$wgExtensionAssetsPath/Maps" ); 
 		}
 		
 		return true;

PrefSwitch/PrefSwitch.hooks.php
@@ -12,8 +12,8 @@ class PrefSwitchHooks {
 	
 	protected static $modules = array(
 		'ext.prefSwitch' => array(
-			'scripts' => 'extensions/PrefSwitch/modules/ext.prefSwitch.js',
-			'styles' => 'extensions/PrefSwitch/modules/ext.prefSwitch.css',
+			'scripts' => 'ext.prefSwitch.js',
+			'styles' => 'ext.prefSwitch.css',
 			'dependencies' => 'jquery.client',
 		),
 	);
@@ -84,8 +84,11 @@ class PrefSwitchHooks {
 	 * ResourceLoaderRegisterModules hook
 	 */
 	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
+		global $wgExtensionAssetsPath;
+		$localpath = dirname( __FILE__ ) . '/modules';
+		$remotepath = "$wgExtensionAssetsPath/PrefSwitch/moduless";
 		foreach ( self::$modules as $name => $resources ) {
-			$resourceLoader->register( $name, new ResourceLoaderFileModule( $resources ) );
+			$resourceLoader->register( $name, new ResourceLoaderFileModule( $resources, $localpath, $remotepath ) );
 		}
 		return true;
 	}

ProofreadPage/ProofreadPage_body.php
@@ -83,11 +83,14 @@ class ProofreadPage {
 	}
 
 	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
+		global $wgExtensionAssetsPath;
+		$localpath = dirname( __FILE__ );
+		$remotepath = "$wgExtensionAssetsPath/ProofreadPage";
 		$resourceLoader->register(
 			'ext.proofreadpage.page',
 			new ResourceLoaderFileModule(
 				array(
-					'scripts' => 'extensions/ProofreadPage/proofread.js',
+					'scripts' => 'proofread.js',
 					'messages' => array(
 						'proofreadpage_header',
 						'proofreadpage_body',
@@ -100,7 +103,7 @@ class ProofreadPage {
 						'proofreadpage_quality3_category',
 						'proofreadpage_quality4_category',
 					)
-				)
+				), $basepath, $remotepath
 			)
 		);
 
@@ -108,14 +111,17 @@ class ProofreadPage {
 			'ext.proofreadpage.article',
 			new ResourceLoaderFileModule(
 				array(
-					'scripts' => 'extensions/ProofreadPage/proofread_article.js'
-				)
+					'scripts' => 'proofread_article.js'
+				), $basepath, $remotepath
 			)
 		);
 
 		$resourceLoader->register(
 			'ext.proofreadpage.index',
-			new ResourceLoaderFileModule( array( 'scripts' => 'extensions/ProofreadPage/proofread_index.js' ) )
+			new ResourceLoaderFileModule(
+				array( 'scripts' => 'proofread_index.js' ),
+				$basepath, $remotepath
+			)
 		);
 
 		return true;

SemanticForms/includes/SF_Utils.php
@@ -106,46 +106,39 @@ END;
 	 * and CSS
 	 */
 	static function registerModules( $resourceLoader ) {
-		global $sfgPartialPath, $smwgScriptPath;
+		global $wgExtensionAssetsPath;
+		$localpath = dirname( dirname( __FILE__ ) );
+		$remotepath = "$wgExtensionAssetsPath/SemanticForms";
 
 		$resourceLoader->register(
 			array(
-				'semanticforms.main' =>
-				new ResourceLoaderFileModule(
+				'semanticforms.main' => new ResourceLoaderFileModule(
 					array(
-						'scripts' =>
-						"$sfgPartialPath/libs/SemanticForms.js",
-						'styles' =>
-						array(
-							"$sfgPartialPath/skins/SemanticForms.css",
-							"$sfgPartialPath/skins/SF_jquery_ui_overrides.css",
+						'scripts' => 'libs/SemanticForms.js',
+						'styles' => array(
+							'skins/SemanticForms.css',
+							'skins/SF_jquery_ui_overrides.css',
 						),
-					)
+					), $localpath, $remotepath
 				),
-				'semanticforms.fancybox' =>
-				new ResourceLoaderFileModule(
+				'semanticforms.fancybox' => new ResourceLoaderFileModule(
 					array(
-						'scripts' =>
-						"$sfgPartialPath/libs/jquery.fancybox-1.3.1.js",
-						'styles' =>
-						"$sfgPartialPath/skins/jquery.fancybox-1.3.1.css",
-					)
+						'scripts' => 'libs/jquery.fancybox-1.3.1.js',
+						'styles' => 'skins/jquery.fancybox-1.3.1.css',
+					), $localpath, $remotepath
 				),
-				'semanticforms.autogrow' =>
-				new ResourceLoaderFileModule(
+				'semanticforms.autogrow' => new ResourceLoaderFileModule(
 					array(
-						'scripts' =>
-						"$sfgPartialPath/libs/SF_autogrow.js",
-					)
+						'scripts' => 'libs/SF_autogrow.js',
+					), $localpath, $remotepath
 				),
-				'semanticforms.smw_utilities' =>
-				new ResourceLoaderFileModule(
+				'semanticforms.smw_utilities' => new ResourceLoaderFileModule(
 					array(
-						'scripts' =>
-						// no $smwgPartialPath variable exists yet
-						"/extensions/SemanticMediaWiki/skins/SMW_tooltip.js",
-						"/extension/SemanticMediaWiki/skins/SMW_sorttable.js",
-					)
+						'scripts' => array(
+							'skins/SMW_tooltip.js',
+							'skins/SMW_sorttable.js',
+						)
+					), $localpath, $remotepath
 				),
 			)
 		);

SemanticMediaWiki/SMW.hooks.php
@@ -29,17 +29,18 @@ final class SMWHooks {
 
 		$modules = array(
 			'ext.smw.style' => array(
-				'styles' =>   ( $wgContLang->isRTL() ? 'SMW_custom_rtl.css' : 'SMW_custom.css' )
+				// FIXME: Won't work right, ResourceLoader does RTL flipping itself
+				'styles' => ( $wgContLang->isRTL() ? 'SMW_custom_rtl.css' : 'SMW_custom.css' )
 			),
 			'ext.smw.tooltips' => array(
-				'scripts' =>   'SMW_tooltip.js',
+				'scripts' => 'SMW_tooltip.js',
 				'dependencies' => array(
 					'mediawiki.legacy.wikibits',
 					'ext.smw.style'
 				)
 			),
 			'ext.smw.sorttable' => array(
-				'scripts' =>  dirname( __FILE__ ) . 'SMW_sorttable.js',
+				'scripts' => 'SMW_sorttable.js',
 				'dependencies' => 'ext.smw.style'
 			)		
 		);
@@ -49,7 +50,7 @@ final class SMWHooks {
 				$name,
 				new ResourceLoaderFileModule(
 					array_merge_recursive( $resources, array( 'group' => 'ext.smw' ) ),
-					'extensions/SemanticMediaWiki/skins/'
+					dirname( __FILE__ ) . '/skins', $smwgScriptPath . '/skins'
 				)
 			); 
 		}

commit 38eebdc5642351ad4db72675f9f2d4cbe263dfdb

Fix Translate breakage in r75587
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@75590 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

Translate/Translate.php
@@ -422,8 +422,9 @@ $wgTranslateYamlLibrary = 'spyc';
 function efTranslateResources( &$resourceLoader ) {
 	global $wgExtensionAssetsPath;
 	$resourceLoader->register( array( 'translate-css' =>
-		new ResourceLoaderFileModule( array( 'styles' => 'Translate.css' ) )
-	), dirname( __FILE__ ), "$wgExtensionAssetsPath/Translate" );
+		new ResourceLoaderFileModule( array( 'styles' => 'Translate.css' ),
+		dirname( __FILE__ ), "$wgExtensionAssetsPath/Translate" )
+	) );
 	return true;
 }
 