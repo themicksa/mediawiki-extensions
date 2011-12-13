themicksa/mediawiki-extensions

commit ee685943bc95e9cc99f2117c2c5b784b271fb4b5

Translation of AFTv5 feedback page error messages, including moving error messages out of the PHP and into the JS. Eventually I suspect all the bucket display logic will wind up there as well.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@105487 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

ArticleFeedbackv5/modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.special.js
@@ -53,18 +53,27 @@
 			'type'    : 'POST',
 			'dataType': 'json',
 			'data'    : {
-				'affeedbackid': id,
-				'afflagtype'  : type,
-				'format' : 'json',
-				'action' : 'articlefeedbackv5-flag-feedback'
+				'feedbackid': id,
+				'flagtype'  : type,
+				'format'    : 'json',
+				'action'    : 'articlefeedbackv5-flag-feedback'
 			},
 			'success': function ( data ) {
-				// TODO check output and error if needed
-				$( '#aft5-' + type + '-link-' + id ).text(
-					mw.msg( 'articlefeedbackv5-' + type + '-saved' )
-				);
+				var msg = 'articlefeedbackv5-error-flagging';
+				if ( 'articlefeedbackv5-flag-feedback' in data ) {
+					if ( 'result' in data['articlefeedbackv5-flag-feedback'] ) {
+						if( data['articlefeedbackv5-flag-feedback'].result == 'Success' ) {
+							msg = 'articlefeedbackv5-' + type + '-saved';
+						} else if (data['articlefeedbackv5-flag-feedback'].result == 'Error' ) {
+							msg = data['articlefeedbackv5-flag-feedback'].reason;
+						}
+					}
+				}
+				$( '#aft5-' + type + '-link-' + id ).text( mw.msg( msg ) );
+			},
+			'failure': function ( data ) {
+				$( '#aft5-' + type + '-link-' + id ).text( mw.msg( 'articlefeedbackv5-error-flagging' ) );
 			}
-			// TODO have a callback for failures.
 		} );
 		return false;
 	}

ArticleFeedbackv5/ArticleFeedbackv5.hooks.php
@@ -132,6 +132,13 @@ class ArticleFeedbackv5Hooks {
 		'jquery.articleFeedbackv5.special' => array(
 			'scripts ' => 'jquery.articleFeedbackv5/jquery.articleFeedbackv5.special.js',
 			'styles'   => 'jquery.articleFeedbackv5/jquery.articleFeedbackv5.special.css',
+			'messages' => array(
+				'articlefeedbackv5-error-flagging',
+				'articlefeedbackv5-invalid-feedback-id',
+				'articlefeedbackv5-invalid-feedback-flag',
+        			'articlefeedbackv5-abuse-saved',
+				'articlefeedbackv5-hide-saved',
+			),
 		),
 	);
 

ArticleFeedbackv5/ArticleFeedbackv5.i18n.php
@@ -23,6 +23,7 @@ $messages['en'] = array(
 	'articlefeedbackv5-error-unknown' => 'Unknown error',
 	'articlefeedbackv5-error-submit' => 'Form submission error.',
 	'articlefeedbackv5-error-nofeedback' => 'Please enter your feedback.',
+	'articlefeedbackv5-error-flagging' => 'Error flagging feedback.',
 
 	// Article Feedback special page.
 	'articlefeedbackv5-form-optionid' => 'Option $1',

ArticleFeedbackv5/api/ApiFlagFeedbackArticleFeedbackv5.php
@@ -15,7 +15,7 @@
  */
 class ApiFlagFeedbackArticleFeedbackv5 extends ApiBase {
 	public function __construct( $query, $moduleName ) {
-		parent::__construct( $query, $moduleName, 'af' );
+		parent::__construct( $query, $moduleName, '' );
 	}
 
 	/**
@@ -28,7 +28,7 @@ class ApiFlagFeedbackArticleFeedbackv5 extends ApiBase {
 
 		if ( !isset( $params['feedbackid'] )
 		 || !preg_match( '/^\d+$/', $params['feedbackid'] ) ) {
-			$error = wfMsg( 'articlefeedbackv5-invalid-feedback-id' );
+			$error = 'articlefeedbackv5-invalid-feedback-id';
 		}
 
 		# load feedback record, bail if we don't have one
@@ -40,13 +40,13 @@ class ApiFlagFeedbackArticleFeedbackv5 extends ApiBase {
 
 		if ( !$record->af_id ) {
 			// no-op, because this is already broken
-			$error = wfMsg( 'articlefeedbackv5-invalid-feedback-id' );
+			$error = 'articlefeedbackv5-invalid-feedback-id';
 		} elseif ( $params['flagtype'] == 'abuse' ) {
 			$update['af_abuse_count'] = $record->af_abuse_count + 1;
 		} elseif ( $params['flagtype'] == 'hide' ) {
 			$update['af_hide_count'] = $record->af_hide_count + 1;
 		} else {
-			$error = wfMsg( 'articlefeedbackv5-invalid-feedback-flag' );
+			$error = 'articlefeedbackv5-invalid-feedback-flag';
 		}
 
 		if ( !$error ) {
@@ -63,7 +63,7 @@ class ApiFlagFeedbackArticleFeedbackv5 extends ApiBase {
 			$reason = $error;
 		} else {
 			$result = 'Success';
-			$reason = 'Feedback saved.';
+			$reason = null;
 		}
 
 		$this->getResult()->addValue(

commit 0457a097084952a225a6ecb5b8e88ec116f236cd

AFTv5 - fix error introduced in r105487, where I had the wrong callback keys in jquery requests - error, not failure.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@106067 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

ArticleFeedbackv5/modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.special.js
@@ -71,7 +71,7 @@
 				}
 				$( '#aft5-' + type + '-link-' + id ).text( mw.msg( msg ) );
 			},
-			'failure': function ( data ) {
+			'error': function ( data ) {
 				$( '#aft5-' + type + '-link-' + id ).text( mw.msg( 'articlefeedbackv5-error-flagging' ) );
 			}
 		} );
@@ -115,7 +115,7 @@
 					$( '#aft5-show-feedback' ).text( mw.msg( 'articlefeedbackv5-error-loading-feedback' ) );
 				}
 			},
-			'failure': function ( data ) {
+			'error': function ( data ) {
 				$( '#aft5-show-feedback' ).text( mw.msg( 'articlefeedbackv5-error-loading-feedback' ) );
 			}
 		} );