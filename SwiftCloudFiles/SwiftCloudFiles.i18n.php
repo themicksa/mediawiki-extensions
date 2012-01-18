c0badd863dbc80f6c6d6522379fea9e2fab5639c
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-18 21:23:01 +0000

    r109227: Consistency tweaks in preparation for adding extension to translatewiki.net
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@109434 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SwiftCloudFiles/SwiftCloudFiles.i18n.php
+++ SwiftCloudFiles/SwiftCloudFiles.i18n.php
@@ -1,7 +1,17 @@
 <?php
+/**
+ * Internationalisation for Add Media Wizard extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
 $messages = array();
 
+/** English
+ * @author Aaron Schulz
+ */
 $messages['en'] = array(
 	'swiftcloudfiles-desc' => "Includes and registers the ''php-cloudfiles'' API (https://github.com/rackspace/php-cloudfiles), allowing MediaWiki to interface with an OpenStack Swift storage backend."
 );
+
