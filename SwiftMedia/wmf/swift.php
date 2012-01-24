31662a054c26b5405f06ccefa8236005e3267d5a
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-24 20:50:06 +0000

    Upated thumbnail purge script to account for latest file backend code changes. Also made it handle sharding of enwiki and commons containers. Finally, fixed delete_object calls, as the list_object paths are already absolute.
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@109950 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- SwiftMedia/wmf/swift.php
+++ SwiftMedia/wmf/swift.php
@@ -3,14 +3,12 @@
  * Helper functions for Swift related image thumbnail purging.
  * The functions here should only be called after MediaWiki setup.
  *
+ * The SwiftCloudFiles extensions must be installed.
  * $wmfSwiftConfig must reside in PrivateSettings.php. It should also
  * be extracted in CommonSettings.php to set any swift backend settings.
  *
  * This file belongs under wmf-config/ and should be included by CommonSettings.php.
  */
-
-# Swift API client module and helper functions
-require_once( '/usr/local/apache/common-local/lib/php-cloudfiles/cloudfiles.php' );
 
 /**
  * Handler for the LocalFilePurgeThumbnails hook.
@@ -24,21 +22,22 @@ require_once( '/usr/local/apache/common-local/lib/php-cloudfiles/cloudfiles.php'
 function wmfPurgeBackendThumbCache( File $file, $archiveName ) {
 	global $site, $lang; // CommonSettings.php
 
+	// Get thumbnail dir relative to thumb zone
 	if ( $archiveName !== false ) {
 		$thumbRel = $file->getArchiveThumbRel( $archiveName ); // old version
 	} else {
 		$thumbRel = $file->getRel(); // current version
 	}
 
-	$container = wmfGetSwiftThumbContainer( $site, $lang );
+	// Get the container for the thumb zone and delete the objects
+	$container = wmfGetSwiftThumbContainer( $site, $lang, "$thumbRel/" );
 	if ( $container ) { // sanity
 		$files = $container->list_objects( 0, NULL, "$thumbRel/" );
 		foreach ( $files as $file ) {
-			$name = "{$thumbRel}/{$file}"; // swift object name
 			try {
-				$container->delete_object( $name );
+				$container->delete_object( $file );
 			} catch ( NoSuchObjectException $e ) { // probably a race condition
-				wfDebugLog( 'swiftThumb', "Could not delete `{$name}`; object does not exist." );
+				wfDebugLog( 'swiftThumb', "Could not delete `{$file}`; object does not exist." );
 			}
 		}
 	}
@@ -51,10 +50,11 @@ function wmfPurgeBackendThumbCache( File $file, $archiveName ) {
  *
  * @param $site string
  * @param $lang string
+ * @param $relPath string Path relative to container
  * @return CF_Container|null
  */
-function wmfGetSwiftThumbContainer( $site, $lang ) {
-	global $wmfSwiftConfig; // PrivateSettings.php
+function wmfGetSwiftThumbContainer( $site, $lang, $relPath ) {
+	global $wmfSwiftConfig; // from PrivateSettings.php
 
 	$auth = new CF_Authentication(
 		$wmfSwiftConfig['user'],
@@ -66,7 +66,19 @@ function wmfGetSwiftThumbContainer( $site, $lang ) {
 
 	$conn = new CF_Connection( $auth );
 
-	$name = "{$site}-{$lang}-media-thumb"; // swift container name
+	$wikiId = "{$site}-{$lang}";
+
+	// Get the full swift container name, including any shard suffix
+	$name = "{$wikiId}-local-thumb";
+	if ( in_array( $wikiId, array( 'wikipedia-commons', 'wikipedia-en' ) ) ) {
+		// Code stolen from FileBackend::getContainerShard()
+		if ( preg_match( "!^(?:[^/]{2,}/)*[0-9a-f]/(?P<shard>[0-9a-f]{2})(?:/|$)!", $relPath, $m ) ) {
+			$name .= '.' . $m['shard'];
+		} else {
+			throw new MWException( "Can't determine shard of path '$relPath' for '$wikiId'." );
+		}
+	}
+
 	try {
 		$container = $conn->get_container( $name );
 	} catch ( NoSuchContainerException $e ) { // container not created yet
