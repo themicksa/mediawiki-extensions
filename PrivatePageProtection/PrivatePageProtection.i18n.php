612126e6c3810b5f219453db4a153c5f8b974824
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-01-31 19:10:14 +0000

    r108717: Consistency tweaks in preparation for adding extension to translatewiki.net * Fix typos in German * Fix language code "de_formal" -> "de-formal"
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@110416 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PrivatePageProtection/PrivatePageProtection.i18n.php
+++ PrivatePageProtection/PrivatePageProtection.i18n.php
@@ -17,7 +17,6 @@ $messages['en'] = array(
 	'privatepp-lockout-prevented' => 'Lockout prevented: You have tried to restrict access to this page to {{PLURAL:$2|the group|one of the groups}} $1. 
 Since you are not a member of {{PLURAL:$2|this group|any of these groups}}, you would not be able to access the page after saving it. 
 Saving was aborted to avoid this.',
-
 );
 
 /** Message documentation (Message documentation)
@@ -33,17 +32,17 @@ $messages['qqq'] = array(
 $messages['de'] = array(
 	'privatepp-desc' => 'Beschränkt den Zugang zu Wikiseite auf der Basis von benutzergruppen.',
 	
-	'privatepp-lockout-prevented' => 'Aussperrung verhindert: Du hast versucht, den Zugang zu dieser Seite auf {{PLURAL:$2|die Gruppe|die gruppen}} $1 zu beschränken. 
+	'privatepp-lockout-prevented' => 'Aussperrung verhindert: Du hast versucht, den Zugang zu dieser Seite auf {{PLURAL:$2|die Gruppe|die Gruppen}} $1 zu beschränken. 
 Da du kein Mitglied {{PLURAL:$2|dieser Gruppe|einer dieser Gruppen}} bist, könntest du nach dem Speichern nicht mehr auf die Seite zugreifen. 
-Um dies zu vermeiden wurde das Speichern abgebrochen.',
+Um dies zu vermeiden, wurde das Speichern abgebrochen.',
 );
 
 /** German (Deutsch)
  * @author Daniel Kinzler
  */
-$messages['de_formal'] = array(
-	'privatepp-lockout-prevented' => 'Aussperrung verhindert: Sie haben versucht, den Zugang zu dieser Seite auf {{PLURAL:$2|die Gruppe|die gruppen}} $1 zu beschränken.
-Da sie kein Mitglied {{PLURAL:$2|dieser Gruppe|einer dieser Gruppen}} sind, könnten sie nach dem Speichern nicht mehr auf die Seite zugreifen.
-Um dies zu vermeiden wurde das Speichern abgebrochen.',
+$messages['de-formal'] = array(
+	'privatepp-lockout-prevented' => 'Aussperrung verhindert: Sie haben versucht, den Zugang zu dieser Seite auf {{PLURAL:$2|die Gruppe|die Gruppen}} $1 zu beschränken.
+Da Sie kein Mitglied {{PLURAL:$2|dieser Gruppe|einer dieser Gruppen}} sind, könnten Sie nach dem Speichern nicht mehr auf die Seite zugreifen.
+Um dies zu vermeiden, wurde das Speichern abgebrochen.',
 );
 
