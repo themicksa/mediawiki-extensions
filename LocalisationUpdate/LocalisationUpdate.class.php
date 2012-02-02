themicksa/mediawiki-extensions

commit d95f38e05fc37b6c83406c02752c9094490161cd

Followup r103763: per Liangent's CR comment, there was a bug where an unchanged message would cause the fallback language to be accessed instead. Per my CR comment, this can be solved by explicitly storing unchanged messages (but not storing messages whose English translations have changed). I've also revised the logic for carrying over stored messages from the cache file: previously messages would never be removed from the cache file, which would cause issues when people deliberately remove messages to let the fallback kick in. The logic has now been changed to remove messages from the LU cache if they've been removed from trunk, but to keep them (at their cached version) if they still exist in trunk but their English translation has changed.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@104041 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

LocalisationUpdate/LocalisationUpdate.class.php
@@ -450,21 +450,30 @@ class LocalisationUpdate {
 			return 0;
 		}
 
-		$new_messages = self::readFile( $langcode );
+		$new_messages = array();
 
-		foreach ( $changedStrings as $key => $value ) {
-			// If this message wasn't changed in English.
-			if ( !isset( $forbiddenKeys[$key] ) ) {
+		//foreach ( $changedStrings as $key => $value ) {
+		// HACK for r103763 CR: store all messages, even unchanged ones
+		// TODO this file is a mess and needs to be rewritten
+		foreach ( array_merge( array_keys( $base_messages, $compare_messages ) as $key ) {
+			// Only update the translation if this message wasn't changed in English
+			if ( !isset( $forbiddenKeys[$key] ) && isset( $base_messages[$key] ) ) {
 				$new_messages[$key] = $base_messages[$key];
 
-				// Output extra logmessages when needed.
-				if ( $verbose ) {
-					$oldmsg = isset( $compare_messages[$key] ) ? "'{$compare_messages[$key]}'" : 'not set';
-					self::myLog( "Updated message {$key} from $oldmsg to '{$base_messages[$key]}'", $verbose );
-				}
+				if ( !isset( $compare_messages[$key] ) || $compare_messages[$key] !== $base_messages[$key] ) {
+					// Output extra logmessages when needed.
+					if ( $verbose ) {
+						$oldmsg = isset( $compare_messages[$key] ) ? "'{$compare_messages[$key]}'" : 'not set';
+						self::myLog( "Updated message {$key} from $oldmsg to '{$base_messages[$key]}'", $verbose );
+					}
 
-				// Update the counter.
-				$updates++;
+					// Update the counter.
+					$updates++;
+				}
+			} else if ( isset( $forbiddenKeys[$key] ) && isset( $compare_messages[$key] ) ) {
+				// The message was changed in English, but a previous translation still exists in the cache.
+				// Use that previous translation rather than falling back to the .i18n.php file
+				$new_messages[$key] = $compare_messages[$key];
 			}
 		}
 		self::writeFile( $langcode, $new_messages );

commit 60387666cb0c90998b1d069b52ac447840ffb3a5

Fix bug 33768, introduced in r104041: saveChanges() needs to be run even if $changedStrings is empty. Not doing so causes fallback weirdness.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@110568 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

LocalisationUpdate/LocalisationUpdate.class.php
@@ -410,14 +410,16 @@ class LocalisationUpdate {
 		$changedStrings = array_diff_assoc( $base_messages, $compare_messages );
 
 		// If we want to save the differences.
-		if ( $saveResults && !empty( $changedStrings ) && is_array( $changedStrings ) ) {
+		// HACK: because of the hack in saveChanges(), we need to call that function
+		// even if $changedStrings is empty. So comment out the $changedStrings checks below.
+		if ( $saveResults /* && !empty( $changedStrings ) && is_array( $changedStrings )*/ ) {
 			self::myLog( "--Checking languagecode {$langcode}--", $verbose );
 			// Save the differences.
 			$updates = self::saveChanges( $changedStrings, $forbiddenKeys, $compare_messages, $base_messages, $langcode, $verbose );
 			self::myLog( "{$updates} messages updated for {$langcode}.", $verbose );
-		} elseif ( $saveResults ) {
+		} /*elseif ( $saveResults ) {
 			self::myLog( "--{$langcode} hasn't changed--", $verbose );
-		}
+		}*/
 
 		self::saveHash( $basefile, $basehash );
 
@@ -629,14 +631,16 @@ class LocalisationUpdate {
 			$changedStrings = array_diff_assoc( $messages, $compare_messages[$language] );
 
 			// If we want to save the changes.
-			if ( $saveResults === true && !empty( $changedStrings ) && is_array( $changedStrings ) ) {
+			// HACK: because of the hack in saveChanges(), we need to call that function
+			// even if $changedStrings is empty. So comment out the $changedStrings checks below.
+			if ( $saveResults === true /*&& !empty( $changedStrings ) && is_array( $changedStrings )*/ ) {
 				self::myLog( "--Checking languagecode {$language}--", $verbose );
 				// The save them
 				$updates = self::saveChanges( $changedStrings, $forbiddenKeys, $compare_messages[$language], $messages, $language, $verbose );
 				self::myLog( "{$updates} messages updated for {$language}.", $verbose );
-			} elseif($saveResults === true) {
+			}/* elseif($saveResults === true) {
 				self::myLog( "--{$language} hasn't changed--", $verbose );
-			}
+			}*/
 		}
 
 		// And log some stuff.