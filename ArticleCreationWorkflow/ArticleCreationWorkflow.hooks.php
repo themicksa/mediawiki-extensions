3daab0599dce7bc97acf09cc848951117366b599
kaldari <kaldari@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-02-03 00:44:46 +0000

    define method so we dont get an error when loading non-existant pages
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions@110638 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ArticleCreationWorkflow/ArticleCreationWorkflow.hooks.php
+++ ArticleCreationWorkflow/ArticleCreationWorkflow.hooks.php
@@ -1,5 +1,10 @@
 <?php /* Article Creation Hooks */
 
 class ArticleCreationHooks {
-
+	
+	public static function loadArticleCreationModules() {
+		// TODO: make this do something.
+		return true;
+	}
+	
 }
